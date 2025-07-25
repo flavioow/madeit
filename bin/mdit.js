2e470fbbeec6c496fb483cef641b1f03fedbd808
Flávio Henrique <flaviohps8@gmail.com>
2025-07-25 18:50:46 -0300

    :wrench: Chore: Adjustments to the CLI entry point to support ESModules
    Fixed the path of the main file in package.json and adapted mdit.js to support the type module.

--- bin/mdit.js
+++ bin/mdit.js
@@ -1,14 +1,20 @@
 #!/usr/bin/env node
 
-process.env.NODE_NO_WARNINGS = "1"
+import { spawn } from "child_process"
+import { fileURLToPath } from "url"
+import path from "path"
 
-const { spawn } = require("child_process")
-const path = require("path")
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 const args = process.argv.slice(2)
 
 const child = spawn("npx", ["tsx", path.resolve(__dirname, "../src/main.ts"), ...args], {
-    stdio: ["inherit", "inherit", "pipe"]
+    stdio: ["inherit", "inherit", "pipe"],
+    env: {
+        ...process.env,
+        NODE_NO_WARNINGS: "1",
+    },
 })
 
 child.stderr.on("data", (data) => {
